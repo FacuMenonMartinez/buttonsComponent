edccb94af3dc6ff0c6f9c66496b54f141e89a50a
Facundo Menon Martinez <facundomenon.m@gmail.com>
2023-09-06 23:43:16 -0300

    rename buttons.css

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import "./Buttons.css";
+import "./buttons.css";
 
 function ButtonComponent({ iconType, alignIcon, typeButton }) {
 
